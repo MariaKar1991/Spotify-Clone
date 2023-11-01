e7c0b44c3376eff93bc36e7a4281db9f182d2680
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 14:31:49 +0200

    Update App.2.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+// Importing CSS file for styling
 import "./App.css";
+
+// Importing React components
 import { LeftMenu } from "./components/LeftMenu";
 import { MainContainer } from "./components/MainContainer";
 import { RightMenu } from "./components/RightMenu";
 
+// Main App component
 function App() {
   return (
+    // App container
     <div className="App">
+      {/* Left menu component */}
       <LeftMenu />
+      
+      {/* Main container component */}
       <MainContainer />
+      
+      {/* Right menu component */}
       <RightMenu />
 
+      {/* Background element */}
       <div className="background"></div>
     </div>
   );
 }
 
+// Exporting the App component as the default export
 export default App;
