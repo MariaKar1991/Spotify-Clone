ebeb388030daa927d082492f1430762f265ce507
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 14:32:51 +0200

    Update index.2.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
+// Importing necessary modules from React and ReactDOM
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Importing CSS file for styling
 import './index.css';
+
+// Importing the main App component
 import App from './App';
+
+// Importing a function to report web vitals
 import reportWebVitals from './reportWebVitals';
 
+// Creating a root using ReactDOM.createRoot and specifying the root element with id 'root'
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the App component within StrictMode for development checks
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Reporting web vitals
 reportWebVitals();
