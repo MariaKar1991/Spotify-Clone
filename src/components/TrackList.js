1ea86b4f95276bf9aef41c3de19c17a05496b8c6
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 14:45:25 +0200

    Update TrackList.2.js

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -6,16 +6,23 @@ import Track from "../img/track.png";
 function TrackList() {
   return (
     <div className="trackList">
+      {/* Top section with track image and name */}
       <div className="top">
+        {/* Track image */}
         <img src={Track} alt="" />
+
+        {/* Track name with artist */}
         <p className="trackName">
           Sample Name <span className="trackSpan">Artist</span>
         </p>
       </div>
+
+      {/* Bottom section with volume icon, volume range input, music note list icon, and desktop icon */}
       <div className="bottom">
         <i>
           <BsFillVolumeUpFill />
         </i>
+        {/* Volume range input */}
         <input type="range" />
         <i>
           <BsMusicNoteList />
