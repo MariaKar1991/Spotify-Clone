98c39a914f4cd422c92e2d0f9e2a83e90d95dc45
Maria Karavazaki <mariaskaravazaki@gmail.com>
2023-11-01 14:41:02 +0200

    Update MenuPlayList.2.js

--- src/components/MenuPlayList.js
+++ src/components/MenuPlayList.js
@@ -6,22 +6,30 @@ import { PlayList } from "./PlayList";
 function MenuPlayList() {
   return (
     <div className="playListContainer">
+      {/* Container for Playlist name and add icon */}
       <div className="nameContainer">
         <p>PlayList</p>
         <i>
+          {/* Icon for adding a new playlist */}
           <FaPlus />
         </i>
       </div>
+
+      {/* Scrollable container for displaying playlists */}
       <div className="playListScroll">
+        {/* Mapping through the PlayList array to display each playlist */}
         {PlayList &&
           PlayList.map((list) => (
             <div className="playList" key={list.id}>
+              {/* Icon representing a music note list */}
               <i className="list">
                 <BsMusicNoteList />
               </i>
 
+              {/* Playlist name */}
               <p>{list.name}</p>
 
+              {/* Icon for deleting the playlist (trash icon) */}
               <i className="trash">
                 <BsTrash />
               </i>
